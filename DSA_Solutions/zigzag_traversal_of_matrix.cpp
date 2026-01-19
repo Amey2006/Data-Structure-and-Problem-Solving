af0a7165b61c2e3d7de11bb6ce7796703fe812ec
Amey2006 <ameymohite2006@gmail.com>
2026-01-19 10:02:16 +0530

    Solved Problem: Zigzag Traversal Of Matrix

---
+++ DSA_Solutions/zigzag_traversal_of_matrix.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int>ans;
+
+        int dir=1;
+        int row=0;
+        int col=0;
+
+        for (int i=0;i<n*m;i++)
+        {
+            ans.push_back(mat[row][col]);
+            if(dir==1)
+            {
+                if (col == m - 1) {
+                row++;
+                dir = -1;
+            }
+            else if(row==0)
+                {
+                    col++;
+                    dir=-1;
+                }
+                else 
+                {
+                    row--;
+                    col++;
+                }
+            }
+            else{
+                if(row==n-1)
+                {
+                    col++;
+                    dir=1;
+                }
+                 else if (col == 0) {
+                row++;
+                dir = 1;
+            }
+                else
+                {
+                    row++;
+                    col--;
+                }
+            }
+        }
+        return ans;
+    }
+};
