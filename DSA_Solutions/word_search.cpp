2ef15fef8134594cf4ae3fe08726206007c14bb0
Amey2006 <ameymohite2006@gmail.com>
2026-01-31 18:43:04 +0530

    Solved Problem : Word search

---
+++ DSA_Solutions/word_search.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<unordered_map>
+class Solution {
+public:
+    int rows, cols;
+
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int index) {
+      
+        if (index == word.length())
+            return true;
+
+       
+        if (i < 0 || j < 0 || i >= rows || j >= cols ||
+            board[i][j] != word[index])
+            return false;
+
+       
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+       bool found =
+            dfs(board, word, i + 1, j, index + 1) || 
+            dfs(board, word, i - 1, j, index + 1) || 
+            dfs(board, word, i, j + 1, index + 1) || 
+            dfs(board, word, i, j - 1, index + 1);   
+
+        board[i][j] = temp;
+
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        rows = board.size();
+        cols = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, i, j, 0))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
