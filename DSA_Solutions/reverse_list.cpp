f223d0d0e08d802b8d89e27357dcdb3db795271a
Amey2006 <ameymohite2006@gmail.com>
2026-02-19 12:23:54 +0530

    Solved Problem : Reverse Linked List

---
+++ DSA_Solutions/reverse_list.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//  Definition for singly-linked list.
+ struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        vector<int>nums;
+        ListNode*temp=head;
+        while(temp!=nullptr)
+        {
+            nums.push_back(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            temp->val=nums[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
