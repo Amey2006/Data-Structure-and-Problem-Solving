817ba1b364b492c137e81d3b1b7c0c67927f013d
Amey2006 <ameymohite2006@gmail.com>
2026-02-27 20:25:22 +0530

    Solved Problem : Majority Element

---
+++ DSA_Solutions/majority_element.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+    public:
+    int majority_element(vector<int>arr)
+    {
+        int n=arr.size();
+        int count=0;
+        int ele=-1;
+        for(int no:arr)
+        {
+            if(count==0)
+            {
+                ele=no;
+                count=1;
+            }
+            else if(no == ele)
+            {
+                count++;
+            }
+            else count--;
+        }
+
+        count=0;
+        for(int i:arr)
+        {
+            if(i==ele)count++;
+        }
+        if(count>n/2)return ele;
+        else return -1;
+    }
+}
