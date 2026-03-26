5791af247679bb6b82781406b251ed63e293d79c
Amey2006 <ameymohite2006@gmail.com>
2026-03-26 19:33:58 +0530

    Solved Problem : Largest Element at least twice of others

---
+++ DSA_Solutions/dominat_index.cpp
@@ -0,0 +1,23 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+         int max1 = -1, max2 = -1, index = -1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                index = i;
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+
+        if (max1 >= 2 * max2)
+            return index;
+        return -1;
+    }
+};
