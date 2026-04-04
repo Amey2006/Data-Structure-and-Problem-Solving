58eab48669ef6c78b366389eda9d96044bf12cba
Amey2006 <ameymohite2006@gmail.com>
2026-04-04 19:32:59 +0530

    Partition Equal Sum

---
+++ DSA_Solutions 2/partition_equal_sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+  public:
+    bool equalPartition(vector<int>& arr) {
+        int sum = 0;
+        for(int x : arr) sum += x;
+
+        if(sum % 2 != 0) return false;
+
+        int target = sum / 2;
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for(int num : arr) {
+            for(int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+};
